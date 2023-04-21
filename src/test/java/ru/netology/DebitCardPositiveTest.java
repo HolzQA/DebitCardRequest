7f22ba666fcbf7cb05646cfff1b9735572226c49
holz_qa <diatmin@gmail.com>
2023-04-21 21:09:25 +0300

    Return Headless-regime

--- src/test/java/ru/netology/DebitCardPositiveTest.java
+++ src/test/java/ru/netology/DebitCardPositiveTest.java
@@ -22,12 +22,12 @@ public class DebitCardPositiveTest {
     }
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
-//        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
     @AfterEach
     void tearDown() {
