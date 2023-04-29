1dd4dfcdc0da1079eaa5073545f667eafeaec2af
holz_qa <diatmin@gmail.com>
2023-04-29 16:09:55 +0300

    Change Test ShouldNotWithoutCheckbox

--- src/test/java/ru/netology/DebitCardNegativeTest.java
+++ src/test/java/ru/netology/DebitCardNegativeTest.java
@@ -126,8 +126,8 @@ public class DebitCardNegativeTest {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79234567233");
         driver.findElement(By.className("button")).click();
-        String expected = "#ff5c5c";
-        String actual = Color.fromString(driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid .checkbox__text")).getCssValue("color")).asHex();
+        Boolean expected = false;
+        Boolean actual = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).isSelected();
         assertEquals(expected, actual);
     }
 }
